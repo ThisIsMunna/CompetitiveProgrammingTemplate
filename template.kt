21de1bcc9457199b7aa4d5c75e5539cfa9fe7f8f
ThisIsMunna <54361224+ThisIsMunna@users.noreply.github.com>
2021-03-25 05:04:18 +0600

    Create template.kt

---
+++ template.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+}
+private fun readInt() = readLine()!!.toInt()
+private fun readLong() = readLine()!!.toLong()
+private fun readFloat() = readLine()!!.toFloat()
+private fun readDouble() = readLine()!!.toDouble()
+private fun readBoolean() = readLine()!!.toBoolean()
+private fun readString() = readLine()!!
+private fun readByte() = readLine()!!.toByte()
+private fun readShort() = readLine()!!.toShort()
+private fun readInts() = readLine()!!.split(" ").map{it.toInt()}
+private fun readLongs() = readLine()!!.split(" ").map{it.toLong()}
+private fun readFloats() = readLine()!!.split(" ").map{it.toFloat()}
+private fun readDoubles() = readLine()!!.split(" ").map{it.toDouble()}
+private fun readBytes() = readLine()!!.split(" ").map{it.toByte()}
+private fun readShorts() = readLine()!!.split(" ").map{it.toShort()}
+private fun readBooleans() = readLine()!!.split(" ").map{it.toBoolean()}
+private fun readStrings() = readLine()!!.split(" ")
